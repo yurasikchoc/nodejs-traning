b042b4cc98fba5cf427833e312ad9e34fc9357ec
Yury Yaroshyk <yurasikchoc@gmail.com>
2016-07-29 14:03:48 +0300

    completed lesson 8. Sessions

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session')
 var HttpError = require('./error').HttpError;
+var config = require('./config')
+var mongoose = require('./lib/mongoose')
+
 
 
 var routes = require('./routes/index');
@@ -19,11 +22,25 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+var MongoStore = require('connect-mongo')(session);
+app.use(session({
+  secret: config.get('session:secret'),
+  key: config.get('session:sid'),
+  cookie: config.get('session:cookie'),
+  store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
+
+app.use(function(req, res, next){
+  req.session.numberOfVisits = req.session.numberOfVisits + 1 || 1;
+  res.send("Visits: " + req.session.numberOfVisits);
+})
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./middleware/sendHttpError'))
 
