db063b644e62bd9bfd43b2dffe636c3ce379ae96
Yury Yaroshyk <yurasikchoc@gmail.com>
2016-07-29 13:27:29 +0300

    completed lesson 7. Web-services. Errors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session')
+var HttpError = require('./error').HttpError;
 
 
 var routes = require('./routes/index');
@@ -24,6 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./middleware/sendHttpError'))
 
 app.use('/', routes);
 app.use('/users', users);
@@ -37,26 +39,24 @@ app.use(function(req, res, next) {
 
 // error handlers
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  if (typeof err == 'number'){
+    err = new HttpError(err);
+  }
+  if (err instanceof HttpError){
+    res.sendHttpError(err);
+  } else {
+    if (app.get('env') === 'development') {
+      res.status(err.status || 500);
+      res.render('error', {
+        message: err.message,
+        error: err
+      })
+    } else {
+      err = new HttpError(500);
+      res.sendHttpError(err);
+    }  
+  } 
 });
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
+var ObjectID = require('mongodb').ObjectID;
+var HttpError = require('../error').HttpError;
 var router = express.Router();
 
 /* GET users listing. */
+
+var User = require('../models/user').User;
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  	User.find({}, function(err, users) {
+  		if (err) return next(err); 
+  		res.json(users)
+  	})
 });
+
+router.get('/:id', function(req, res, next) {
+  	try {
+      var id = new ObjectID(req.params.id);
+    } catch (e) {
+      return next(404);
+    }  
+    User.findById(req.params.id, function(err, user) {
+  		if (err) return next(err); 
+  		if (!user) {
+  			next(new HttpError(404, 'User not found'));
+  		} else {
+  		  res.json(user)
+      }
+  	})
+});
+
 
 module.exports = router;
